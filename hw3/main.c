92b83dec23f67a8e95341231d20c358cfcad0c4f
dimstf <dmistf@gmail.com>
2023-06-14 18:35:09 +0700

    Fix Homework 3

--- hw3/main.c
+++ hw3/main.c
@@ -4,12 +4,19 @@ int main()
 {
     // 1 Third byte
 	int num = 0xDDCCBBAA;
-	int byte = 3;
-	unsigned char c = ((num >> (8 * (byte - 1)) ) & 0xFF);
-    printf("Byte №%d of number 0x%X = %X\n", byte, num, c);
+	int byte = 1;
+	unsigned char c=1;
+    
+	while(byte<=sizeof(int))
+	{
+		c = ((num >> (8 * (byte - 1)) ) & 0xFF);
+		printf("Byte №%d of number 0x%X = %X\n", byte, num, c);
+		byte++;
+	}
 	
 	// 2 CC OR 22 = EE
-	printf("result = %X\n",num | 0x220000);
+	printf("Change CC on EE:\n");
+	printf(" 0x%X | 0x220000 = %X\n", num, num | 0x220000);
 	
 	return 0;
 }
