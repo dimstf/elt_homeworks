e161fcde241ff80cba16a86b5b20bcc2dc00f903
dimstf <dimstf@DIMON.localdomain>
2023-06-28 15:12:14 +0700

    Homework 10

---
+++ hw10/src/calc.c
@@ -0,0 +1,23 @@
+double calculate_addition(double arg1, double arg2)
+{
+	double result=arg1+arg2;
+	return result;
+}
+
+double calculate_subtraction(double arg1, double arg2)
+{
+	double result=arg1-arg2;
+	return result;
+}
+
+double calculate_multiplication(double arg1, double arg2)
+{
+	double result=arg1*arg2;
+	return result;
+}
+
+double calculate_division(double arg1, double arg2)
+{
+	double result=arg1/arg2;
+	return result;
+}
