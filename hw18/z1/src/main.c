fe66eee06196e537adbff83bd37d8c6c39ea7f81
dimstf <dimstf@DIMON.localdomain>
2023-07-20 17:27:00 +0700

    Fixed homework 18

--- hw18/z1/src/main.c
+++ hw18/z1/src/main.c
@@ -6,12 +6,14 @@
 
 int main()
 {
-	int pp[2],i;
+	int pp1[2],pp2[2],i;
 	pid_t pid;
 
-	if(pipe(pp)<0)
+	if(pipe(pp1)<0)
 		exit(1);
-
+	if(pipe(pp2)<0)
+		exit(1);
+	
 	pid=fork();
 	switch(pid)
 	{
@@ -25,17 +27,27 @@ int main()
 			char outbuf[MSGSIZE];
 			for(i=0;i<2;i++)
 			{
-				read(pp[0],outbuf,MSGSIZE);
+				read(pp1[0],outbuf,MSGSIZE);
 				printf("%s\n",outbuf);
 			}
+			char msg1[]="hello 3";
+			char msg2[]="hello 4";
+			write(pp2[1],msg1,MSGSIZE);
+			write(pp2[1],msg2,MSGSIZE);
 			exit(0);
 		} break;
 		default:
 		{
 			char msg1[]="hello 1";
 			char msg2[]="hello 2";
-			write(pp[1],msg1,MSGSIZE);
-			write(pp[1],msg2,MSGSIZE);
+			write(pp1[1],msg1,MSGSIZE);
+			write(pp1[1],msg2,MSGSIZE);
+			char outbuf[MSGSIZE];
+			for(i=0;i<2;i++)
+			{
+				read(pp2[0],outbuf,MSGSIZE);
+				printf("%s\n",outbuf);
+			}
 			int st;
 			wait(&st);
 		} break;
