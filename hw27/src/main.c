b3aa2ed8c51af22fe759e21beca73cecdcf7ce6e
dimstf <dimstf@mail.ru>
2023-08-23 21:36:25 +0000

    Fix homework 27

--- hw27/src/main.c
+++ hw27/src/main.c
@@ -1,12 +1,16 @@
-#include <arpa/inet.h>
-#include <linux/if_packet.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
 #include <net/if.h>
-#include <netinet/ether.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
 
 #define MY_DEST_MAC0 0x00
 #define MY_DEST_MAC1 0x00
@@ -16,7 +20,6 @@
 #define MY_DEST_MAC5 0x00
 
 #define DEFAULT_IF "enp5s0"
-#define BUF_SIZE 1024
 
 void print_error(char *error_msg,int exit_status)
 {
@@ -24,37 +27,55 @@ void print_error(char *error_msg,int exit_status)
     exit(exit_status);
 }
 
-int main(int argc,char *argv[])
+unsigned short csum(unsigned short *buf, int nwords)
+{
+    unsigned long sum;
+    for(sum=0;nwords>0;nwords--)
+    sum+=*buf++;
+    sum=(sum >> 16)+(sum &0xffff);
+    sum+=(sum>>16);
+    return (unsigned short)(~sum);
+}
+
+int main(int argc, char *argv[])
 {
     int sock;
     struct ifreq if_idx;
     struct ifreq if_mac;
-    int tx_len=0;
-    char buf[BUF_SIZE];
-    struct ether_header *eh=(struct ether_header *)buf;
-    struct iphdr *iph=(struct iphdr *)(buf + sizeof(struct ether_header));
+    struct ifreq ifreq_ip;
+    int len=0;
+    unsigned char* buf;
+    buf=(unsigned char*)malloc(64); 
+    memset(buf,0,64);
+
+    struct ether_header *eh=(struct ether_header*)buf;
+    struct iphdr *iph=(struct iphdr*)(buf+sizeof(struct ether_header));
+    struct udphdr *uph=(struct udphdr*)(buf+sizeof(struct ether_header)+sizeof(struct iphdr));
     struct sockaddr_ll addr;
     char ifName[IFNAMSIZ];
-
+    
     if(argc>1)
-	strcpy(ifName,argv[1]);
+        strcpy(ifName,argv[1]);
     else
-	strcpy(ifName,DEFAULT_IF);
+        strcpy(ifName,DEFAULT_IF);
 
     if((sock=socket(AF_PACKET,SOCK_RAW,IPPROTO_RAW))==-1)
-	print_error("Socket error. Exit.",1);
+        print_error("Socket error. Exit.",1);
 
     memset(&if_idx,0,sizeof(struct ifreq));
     strncpy(if_idx.ifr_name,ifName,IFNAMSIZ-1);
-    if(ioctl(sock, SIOCGIFINDEX, &if_idx)<0)
-	print_error("Ioctl error 1. Exit",1);
+    if(ioctl(sock,SIOCGIFINDEX,&if_idx)<0)
+	print_error("Ioctl 1 error. Exit.",1);
 
     memset(&if_mac,0,sizeof(struct ifreq));
     strncpy(if_mac.ifr_name,ifName,IFNAMSIZ-1);
     if(ioctl(sock,SIOCGIFHWADDR,&if_mac)<0)
-	print_error("Ioctl error 2. Exit",1);
+	print_error("Ioctl 2 error. Exit.",1);
 
-    memset(buf,0,BUF_SIZE);
+    memset(&ifreq_ip,0,sizeof(ifreq_ip));
+    strncpy(ifreq_ip.ifr_name,ifName,IFNAMSIZ-1);
+    if(ioctl(sock,SIOCGIFADDR,&ifreq_ip)<0)
+	print_error("Ioctl 3 error. Exit.",1);
 
     eh->ether_shost[0]=((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[0];
     eh->ether_shost[1]=((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[1];
@@ -68,26 +89,51 @@ int main(int argc,char *argv[])
     eh->ether_dhost[3]=MY_DEST_MAC3;
     eh->ether_dhost[4]=MY_DEST_MAC4;
     eh->ether_dhost[5]=MY_DEST_MAC5;
-    eh->ether_type=htons(ETH_P_IP);
-    tx_len+=sizeof(struct ether_header);
-	
-    buf[tx_len++] = 0xde;
-    buf[tx_len++] = 0xad;
-    buf[tx_len++] = 0xbe;
-    buf[tx_len++] = 0xef;
+    eh->ether_type=8;
+    len+=sizeof(struct ether_header);
+
+    iph->ihl=5;
+    iph->version=4;
+    iph->tos=0;
+    iph->tot_len=sizeof(struct iphdr)+sizeof(struct udphdr);
+    iph->id=htonl(54321);
+    iph->frag_off=0;
+    iph->ttl=255;
+    iph->protocol=IPPROTO_UDP;
+    iph->check=0;
+    //iph->saddr=inet_addr(inet_ntoa((((struct sockaddr_in*)&(ifreq_ip.ifr_addr))->sin_addr)));    
+    iph->saddr=inet_addr("127.0.0.1");    
+    iph->daddr=inet_addr("127.0.0.1");
     
+    iph->check=csum((unsigned short *)buf,iph->tot_len);
+    len+=sizeof(struct iphdr);
+
+    uph->source=htons(80);
+    uph->dest=htons(43521);
+    uph->check=0;
+
+    len+=sizeof(struct udphdr);
+    buf[len++]='H';
+    buf[len++]='e';
+    buf[len++]='l';
+    buf[len++]='l';
+    buf[len++]='o';
+    buf[len++]='!';
+    uph->len=htons((len-sizeof(struct iphdr)-sizeof(struct ethhdr)));
+
     addr.sll_ifindex=if_idx.ifr_ifindex;
     addr.sll_halen=ETH_ALEN;
-
     addr.sll_addr[0]=MY_DEST_MAC0;
     addr.sll_addr[1]=MY_DEST_MAC1;
     addr.sll_addr[2]=MY_DEST_MAC2;
     addr.sll_addr[3]=MY_DEST_MAC3;
     addr.sll_addr[4]=MY_DEST_MAC4;
     addr.sll_addr[5]=MY_DEST_MAC5;
-
-    if(sendto(sock,buf,tx_len,0,(struct sockaddr*)&addr,sizeof(struct sockaddr_ll))<0)
-	print_error("Sendto error. Exit.",1);
-
+	
+    int len_ll=sizeof(struct sockaddr_ll);
+    if(sendto(sock,buf,len,0,(struct sockaddr*)&addr,len_ll)<0)
+	print_error("Send error. Exit.",1);
+    close(sock);
+	
     return 0;
 }
